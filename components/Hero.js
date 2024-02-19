30de0dae93f6ee4ac142e233cf3fc2546cf93a24
sunil-b19 <157019894+sunil-b19@users.noreply.github.com>
2024-02-19 10:42:43 +0545

    add video in hero section

--- components/Hero.js
+++ components/Hero.js
@@ -1,32 +1,23 @@
-import React from 'react'
+import React from 'react';
 
 const Hero = () => {
   return (
-    <section className='relative py-8 pb-[3.2rem] lg:py-15 mt-[6rem] lg:mt-[7rem]'>
-      <div className='hero-image'>
+    <section className='relative py-15  mt-[4rem] xs:mt-[5rem]'>
+      {/* <div className='hero-image'></div> */}
+      <div className='w-full h-full mx-auto'>
+        <video className="w-full h-full" loop autoPlay muted>
+          <source src="hero-section-video.mp4" type="video/mp4" className='w-full' />
+        </video>
       </div>
-      <div className='containers relative top-0 left-0'>
-        <div className='hero-content max-w-[750px]'>
-          <h2 className='text-[2rem] sm:text-[2.55rem] lg:text-[3.25rem] font-[900] text-[#20396e] mb-6'>Digital, Business, &
-            Technology Partners for
-            Success</h2>
-          <p className='text-[1.125rem] sm:text-[1.25rem] lg:text-[1.75rem] font-[600] text-[#151719] text-left'>We simplify the digital
-            transformation journeys of
-            businesses through smart and
-            innovative software solutions.</p>
-          <button className='hover-animation-btn bg-[#184ca1] after:bg-[#059a82] mt-5 px-[2rem]'>Contact Our Experts</button>
-        </div>
-      </div>
-      <a href="#">
+      {/* <a href="#">
         <div className="arrow-down">
           <div className="chevron"></div>
           <div className="chevron"></div>
           <div className="chevron"></div>
         </div>
-      </a>
-
+      </a> */}
     </section>
-  )
+  );
 }
 
-export default Hero
+export default Hero;
